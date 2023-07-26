c0bd3f9002e47b107563793a3d83c018d3d36348
DeconstructionMechanics <easonman2003@hotmail.com>
2023-07-26 10:35:36 +0800

    gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -38,8 +38,8 @@ void show_image(Raster::Rasterizer& raster, std::optional<std::string> filename 
 int main(){
     Raster::Rasterizer rasterizer(".\\model\\cow\\spot_triangulated.obj",Raster::ImageColor::BLACKWHITE,500,400);
     std::cout << "load complete" << std::endl;
-    Eigen::Vector3f position(1,0,0);
-    Eigen::Vector3f direction(-1,0,0);
+    Eigen::Vector3f position(0,0.5,-1);
+    Eigen::Vector3f direction(0,0,1);
     rasterizer.camera.config(Raster::Camera::Projection::FISHEYE,Raster::ImageColor::BLACKWHITE,500,400,PI/2,position,direction);
     std::cout << "rendering" << std::endl;
     rasterizer.paint_frame_simple(true);
